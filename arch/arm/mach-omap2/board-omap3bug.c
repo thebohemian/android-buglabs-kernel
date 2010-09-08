6a792217687d51f6dbc708378b28c422b269855e
Matt Isaacs <izzy@buglabs.net>
2010-09-08 16:58:22 -0400

    Machine ID temporary fix..
    Signed-off-by: Matt Isaacs <izzy@buglabs.net>

--- arch/arm/mach-omap2/board-omap3bug.c
+++ arch/arm/mach-omap2/board-omap3bug.c
@@ -1032,8 +1032,8 @@ static void __init omap3_bug_map_io(void)
 	omap2_map_common_io();
 }
 
-MACHINE_START(BUG, "OMAP3 BUG")
-//MACHINE_START(OMAP3EVM, "OMAP3 BUG")
+//MACHINE_START(BUG, "OMAP3 BUG")
+MACHINE_START(OMAP3EVM, "OMAP3 BUG")
 	/* Maintainer: Matt Isaacs - BugLabs, inc */
 	.phys_io	= 0x48000000,
 	.io_pg_offst	= ((0xd8000000) >> 18) & 0xfffc,
