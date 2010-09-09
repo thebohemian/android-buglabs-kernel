79482ff3c8edc366e2088c2e9d7399971abc980a
Matt Isaacs <izzy@buglabs.net>
2010-09-09 10:59:56 -0400

    libertas_tf : Fix oops caused by null hw_reset_device hook.
    Signed-off-by: Matt Isaacs <izzy@buglabs.net>

--- drivers/net/wireless/libertas_tf/main.c
+++ drivers/net/wireless/libertas_tf/main.c
@@ -370,7 +370,7 @@ static int lbtf_op_start(struct ieee80211_hw *hw)
 	return 0;
 
 err_prog_firmware:
-	if (priv->hw_reset_device(card))
+	if (priv->hw_reset_device)
 		priv->hw_reset_device(card);
 	lbtf_deb_leave_args(LBTF_DEB_MACOPS, "error programing fw; ret=%d", ret);
 	return ret;
